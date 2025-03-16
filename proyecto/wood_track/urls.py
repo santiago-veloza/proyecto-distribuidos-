0b805cc211501466775bb395ddd7b0165b3e07fe
santiago-veloza <144069030+santiago-veloza@users.noreply.github.com>
2025-03-15 20:10:20 -0500

    inicio de seccion y guarda user en base de datos y conexxion de varios dispositivos

--- proyecto/wood_track/urls.py
+++ proyecto/wood_track/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, login_view, logout_view, register_view
 
 urlpatterns = [
-    path('', home, name='index.html'),
+    path('', home, name='home'), 
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path('register/', register_view, name='register'),
 ]
--- proyecto/wood_track/views.py
+++ proyecto/wood_track/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+#views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
+@login_required(login_url='/wood_track/login/')
 def home(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('login') 
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST) 
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home') 
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
+def logout_view(request):
+    logout(request)
+    return redirect('login') 
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
